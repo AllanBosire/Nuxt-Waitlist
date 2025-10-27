f3ef72dfc8e9273de24ba8c2f13e1d8b1dd925db
R-Odhiambo <rusell.odhiambo@ifkafin.com>
2025-10-27 09:02:10 +0000

    Blacklisted test domains in Mattermost invite emails

--- server/api/count.get.ts
+++ server/api/count.get.ts
@@ -1,10 +1,19 @@
-import { count } from "drizzle-orm";
+import { count, notLike } from 'drizzle-orm';
+
+const excludedDomains = ['ifkafin.com', 'localhost'];
 
 export default defineEventHandler(async () => {
-	const entry = await useDrizzle().select({ count: count() }).from(tables.waitlist);
+  const entry = await useDrizzle()
+    .select({ count: count() })
+    .from(tables.waitlist)
+    .where(
+      and(
+        ...excludedDomains.map((d) => notLike(tables.waitlist.email, `%${d}`))
+      )
+    );
 
-	if (entry.length === 1) {
-		return entry[0];
-	}
-	return null;
+  if (entry.length === 1) {
+    return entry[0];
+  }
+  return null;
 });
